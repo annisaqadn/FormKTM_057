86fe694755c2310bd74f64da9fc5d8ff4edbb85f
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2022-11-11 19:44:42 +0700

    first commit

---
+++ src/test/java/WSA/project/formKTM/FormKtmApplicationTests.java
@@ -0,0 +1,13 @@
+package WSA.project.formKTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
