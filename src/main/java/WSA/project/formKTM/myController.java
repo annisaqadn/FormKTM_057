953a9c38461ee4b1ec2646de6604b55a9ed7ff63
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2022-11-13 02:01:28 +0700

    Menambahkan isian Controller dan mencoba menampilkan tipe date dan file di controller dulu

--- src/main/java/WSA/project/formKTM/myController.java
+++ src/main/java/WSA/project/formKTM/myController.java
@@ -5,10 +5,36 @@
  */
 package WSA.project.formKTM;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author fuadi
  */
+@Controller
 public class myController {
-    
+    @RequestMapping("/data")
+    @ResponseBody
+    public String getData(@RequestParam("varNama")String textNama,
+                          @RequestParam("varNIM")String texNIM,
+                          @RequestParam("varTL")@DateTimeFormat(pattern="yyyy-MM-dd")Date date,
+                          @RequestParam("varPS")String textPS,
+                          @RequestParam("varFoto")MultipartFile foto)
+            throws IOException{
+        SimpleDateFormat tanggal = new SimpleDateFormat("EEEE-dd-MMMM-yyyy");
+        String tanggalLahir = tanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(foto.getBytes());
+        
+        return "<br> <img src='data:image/*;base64,"+blob+"' /><br>"+tanggalLahir;
+    }
 }
